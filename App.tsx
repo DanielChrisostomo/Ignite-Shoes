abb3d9245db61ca4972de181bae8c3ddd06a3f25
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-06-19 17:49:02 -0300

    OneSignal Inicializado e permissões definidas em App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
@@ -8,6 +9,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("4a77bc0f-d9d0-44c9-9da7-572246380a91")
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
