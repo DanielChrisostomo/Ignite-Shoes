bce4aa1d90bbd52cf39f51632ab67d21b2d6ae52
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-06-24 10:36:08 -0300

    configs em index.tsx routes | Deep Linking na Push Notifications

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,20 @@ import { OneSignal, NotificationWillDisplayEvent, OSNotification } from 'react-n
 import { AppRoutes } from './app.routes';
 import { Notification } from "../components/Notification";
 
+const linking = {
+  prefixes: ["igniteshoes://", "com.rocketseat.igniteshoes://"],
+  config: {
+    screens: {
+      details: {
+        path: "/details/:productId",
+        parse: {
+          productId: (productId: string) => productId, 
+        }
+      }
+    }
+  }
+}
+
 export function Routes() {
   const [notification, setNotification] = React.useState<OSNotification>()
   const { colors } = useTheme();
@@ -28,7 +42,7 @@ export function Routes() {
 
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       {notification?.title && <Notification data={notification} onClose={() => setNotification(undefined)} />} 
